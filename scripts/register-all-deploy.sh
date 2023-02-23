dc5b8acdb9163de7d7d2d36882efebb6657c4119
zackynson <crys.chb@hotmail.com>
2023-02-23 17:25:12 -0300

    refactor: removing unused code

--- scripts/register-all-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-set -u
-: "$STACK_GLOB"
-echo "Deploy $STACK_GLOB stacks"
-echo "$STACK_GLOB" > .cdk-deploy-registered
--- scripts/register-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-set -u
-: "$STACK_PATH"
-: "$STAGE"
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PROJECT="$SCRIPT_DIR/tsconfig.json"
-echo "📝 Registering affected stack from project: $STACK_PATH"
-npx ts-node -r tsconfig-paths/register --project $PROJECT -p -e "require('./scripts/cdk-class-to-project').cdkClassToProject('$STACK_PATH', true)" >> .cdk-deploy-registered
